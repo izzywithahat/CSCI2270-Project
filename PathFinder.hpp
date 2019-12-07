e4b1bbe46c08e7ff83a2b9baee22c5fd5e8edf69
Izzy Russo <isabel.russo@colorado.edu>
2019-12-06 20:42:58 -0700

    Create PathFinder.hpp
    Building the struct for the path finder so that we are able to have pointers to check every direction.
    Start of the class , but still needs the functions that are going to be used.

---
+++ PathFinder.hpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namesapce std;
+
+struct Path
+{
+	Path *NPC = NULL;	
+	Path *northChild = NULL;
+	Path *southChild = NULL;
+	Path *westChild = NULL;
+	Path *eastChild = NULL;
+	Path *prev = NULL;
+};
+
+class PathFinder
+{
+	public:
+		path();
+		~path();
+
+
+	private:
+		Path *head = NULL;
+};
