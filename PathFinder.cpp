6e852ec4dc067c224eac788c51626c423e176bd4
Izzy Russo <isabel.russo@colorado.edu>
2019-12-07 14:29:13 -0700

    Update PathFinder.cpp
    Created the layout for the function definitions. Created the entire function for Displaying the linked list.

--- PathFinder.cpp
+++ PathFinder.cpp
@@ -1,7 +1,50 @@
-PathFinder::path(){}
-PathFinder::~path(){}
-void PathFinder::ConstructGraph(ifstream& inFile){}
-void PathFinder::SearchPaths(){}
-void PathFinder::DisplayLL(){}
-void PathFinder::DisplayPath(int index){}
-void PathFinder::SavePath(ifstream& inFile, int index){}
+#include "PathFinder.hpp"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+PathFinder PathFinder;
+
+PathFinder::PathFinder()
+{
+
+}
+
+PathFinder::~PathFinder()
+{
+
+}
+
+void PathFinder::ConstructGraph(ifstream& inFile)
+{
+
+}
+
+void PathFinder::SearchPaths()
+{
+
+}
+
+void PathFinder::DisplayLL()
+{
+	int count = 0;
+	LLPath* temp = head;
+	while(temp != NULL)
+	{
+		cout << "Option: " << count << " Distance: " temp->dist << endl;
+		temp = temp->next;
+		count++;
+	}
+}
+
+void PathFinder::DisplayPath(int index)
+{
+
+}
+
+void PathFinder::SavePath(ifstream& inFile, int index)
+{
+
+}
