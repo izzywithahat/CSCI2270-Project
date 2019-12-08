e699ef16b7680116234b9831a2f181092537b3e5
Izzy Russo <isabel.russo@colorado.edu>
2019-12-07 22:28:10 -0700

    Update PathFinder.cpp
    Created the skeleton code for searching for paths.

--- PathFinder.cpp
+++ PathFinder.cpp
@@ -21,37 +21,67 @@ void PathFinder::ConstructGraph(ifstream& inFile)
 {
   string line;
   string token;
-  int x=0;
-  int y=0;
+  int x = 0;
+  int y = 0;
   Path mat[18][16];
-  while(getline(inFile,line)){ //Copy Map File onto Graph Matrix
+  while(getline(inFile, line)) //Copy Map File onto Graph Matrix
+  {
     stringstream ss(line);
-    while(ss>>token){
-      mat[y][x]->type=token;
-      mat[y][x]->x=x;
-      mat[y][x]->y=y;
+    while(ss >> token)
+    {
+      mat[y][x]->type = token;
+      mat[y][x]->x = x;
+      mat[y][x]->y = y;
       x++;
     }
     y++;
   }
   //Create Graph From Matrix
-  for(x=0;x<16;x++){
-    for(y=0;y<18;y++){
-      if(x-1>=0)
-        mat[y][x]->WChild=mat[y][x-1];
-      if(x+1<16)
-        mat[y][x]->EChild=mat[y][x+1];
-      if(y-1>=0)
-        mat[y][x]->NChild=mat[y-1][x];
-      if(y+1<18)
-        mat[y][x]->SChild=mat[y+1][x];
+  for(x = 0; x < 16; x++)
+  {
+    for(y = 0; y < 18; y++)
+    {
+      if(x - 1 >= 0)
+        mat[y][x]->WChild = mat[y][x-1];
+      if(x + 1 < 16)
+        mat[y][x]->EChild = mat[y][x+1];
+      if(y - 1 >= 0)
+        mat[y][x]->NChild = mat[y-1][x];
+      if(y + 1 < 18)
+        mat[y][x]->SChild = mat[y+1][x];
     }
   }
 }
 
-void PathFinder::SearchPaths()
+void searchHelper()
 {
 
+}
+
+void PathFinder::SearchPaths()
+{
+	// create dynamic array at root
+	// copy and add to array at each step
+	// if 3 is found copy to linked list then deallocate
+	// else deallocate
+	bool found = false;
+	int capacity = 50;
+	Path* temp = root;
+	PathQ* temp2; 
+	temp2 = new PathQ[capacity];
+
+	while(found)
+	{
+		if(temp->WChild == 3 || temp->SChild == 3 || temp->EChild == 3 || temp->NChild == 3)
+		{
+
+			found = true;
+		}
+		else
+		{
+
+		}
+	}
 }
 
 void PathFinder::DisplayLL()
